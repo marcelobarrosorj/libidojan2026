faa4a1e1343f77f8652cf626b9aeb507dffafbd1
marcelobarrosorj <marcelobarrosorj@gmail.com>
2026-04-09 20:16:21 -0300

    Update SubscriptionNative.tsx

--- SubscriptionNative.tsx
+++ SubscriptionNative.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 const SubscriptionNative = () => {
   return (
     <div style={{ padding: 40, textAlign: 'center', color: '#666' }}>
-      <h3>Pagamento Web Ativo</h3>
-      <p>O componente nativo foi desativado para evitar conflitos.</p>
-      <p>Use o SubscribeButtons acima.</p>
+      <h3>Componente Nativo Desativado</h3>
+      <p>Usando apenas SubscribeButtons para evitar conflitos.</p>
     </div>
   )
 }
