66a2ed48d29ee3d2798dbe714a04bb7b7304de40
marcelobarrosorj <marcelobarrosorj@gmail.com>
2026-04-17 13:06:51 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,11 @@
-
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // se existir
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
-}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
